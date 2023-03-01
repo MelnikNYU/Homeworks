c825f013c4fc0695ef35c8a439d4778319ad2cf2
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-03-01 20:23:59 +0300

    Test 34

---
+++ HWC#1-1/Задача 25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//Math.Pow( x , y );
+
+Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine ()!);
+
+Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine ()!);
+
+double pow = Math.Pow(x,y);
+
+Console.Write($"{pow} ");
---
+++ HWC#1-1/Задача 27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSum(int A)
+{
+int sum = 0;
+int i = 0;
+i = A/1000 + A/100%10 + (A/10%10) + A%10;
+{
+sum += i; // sum = sum + i
+}
+return sum;
+}
+
+//Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма чисел {N} равна {GetSum(N)}");
