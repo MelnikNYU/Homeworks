7e5132cd26281294fa179287e3aa11dbf470f909
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-03-01 22:15:39 +0300

    Test 38

---
+++ HWC#1-1/Задача 38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+double[] GetArray(int size, double minValue, double maxValue)
+{
+    double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+    }
+    return result;
+}
+
+double minN(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double maxN(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+double[] array = GetArray(4, 1, 100);
+Console.WriteLine(String.Join(", ", array));
+double Diff = maxN(array) - minN(array);
+
+Console.WriteLine($"{Diff}"!);
