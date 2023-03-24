c753a78bc9fd87244a5a8626fb525bf9e61bf5dc
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-03-24 23:56:22 +0300

    контрольная 2 варианта

---
+++ HWC#1-1/Контрольная 1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива формирует массив 
+//из элементов менее 3 символов.
+
+
+// var elements = new string[] { "hello", "world", "2224", ":-)1" };
+ 
+// string[] group = Array.FindAll(elements, element => element.Length < 4);
+
+// foreach (var element in group) Console.WriteLine(element);
+string[] array1 = new string[5] {":-)", "03", "hello", "world", "rrrr"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[i] = array1[i];
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
